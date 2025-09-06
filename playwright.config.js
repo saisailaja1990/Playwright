dd9ce326c240c8d15ff12afc2c948645cfc97397
saisailaja1990 <pattipatisaisailaja7@gmail.com>
2025-09-06 19:49:11 +0200

    update playwright.config.js file to run only the testcases saved as spec.js and comment it with //

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,7 @@ import allureReporter from 'allure-playwright';
  */
 export default defineConfig({
   testDir: './tests',
+  //testMatch:['tests/**/*.spec.js','tests-examples/**/*/spec.js'],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
