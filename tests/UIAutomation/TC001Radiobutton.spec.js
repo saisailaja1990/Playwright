3527ee7499387959250dd95307dd00ee4c05a1c0
saisailaja1990 <pattipatisaisailaja7@gmail.com>
2025-09-06 23:41:27 +0200

    add

--- tests/UIAutomation/TC001Radiobutton.spec.js
+++ tests/UIAutomation/TC001Radiobutton.spec.js
@@ -16,8 +16,8 @@ test('Checkboxes', async ({ page }) => {
   }
 
 const checkbox = page.locator('input[id="sunday"]', { timeout: 5000 });
-await checkbox.waitFor({ state: 'visible' }); // Add this line
-await expect(checkbox).toBeVisible();
+//await checkbox.waitFor({ state: 'visible' }); // Add this line
+//await expect(checkbox).toBeVisible();
 await checkbox.check(); // Confirm it's visible
 await expect(checkbox).toBeChecked();
 
