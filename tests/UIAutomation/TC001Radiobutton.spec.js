1cf1d2f1db7c3eedff762febc9614171dac4e3a0
saisailaja1990 <pattipatisaisailaja7@gmail.com>
2025-09-07 06:40:32 +0200

    done

--- tests/UIAutomation/TC001Radiobutton.spec.js
+++ tests/UIAutomation/TC001Radiobutton.spec.js
@@ -15,7 +15,7 @@ test('Checkboxes', async ({ page }) => {
     console.log('Cookie banner not visible.');
   }
 
-const checkbox = page.locator('input[id="sunday"]','{ timeout: 50000 }');
+const checkbox = page.locator('input[id="sunday"]','{ timeout: 500git00 }');
 //await checkbox.waitFor({ state: 'visible' }); // Add this line
 //await expect(checkbox).toBeVisible();
 await checkbox.check(); // Confirm it's visible
