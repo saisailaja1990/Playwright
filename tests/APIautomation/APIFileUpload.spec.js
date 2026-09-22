ddb467d9f86af85260cdc4dfccc485a92c51c6a1
saisailaja1990 <pattipatisaisailaja7@gmail.com>
2026-09-22 11:24:08 +0200

    Updated main.yaml to run api file uplaod

--- tests/APIautomation/APIFileUpload.spec.js
+++ tests/APIautomation/APIFileUpload.spec.js
@@ -21,7 +21,7 @@ test.describe.serial('File Upload and  Download', () => {
                 multipart: {
                     file: {
                         name: 'Test2.txt',
-                        mimeType: 'text/plain',
+                        mimeType: 'text/plain',// browser MIME type in browser based on the file extension
                         buffer: fs.readFileSync('C:/Playwright/tests/uploadFiles/libraryAPI complexjson.txt')//C:\Playwright\tests\uploadFiles\libraryAPI complexjson.txt
                     }
                 }
